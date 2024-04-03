cacf93956880740b13c7571085b680baa904c21c
임수현 <ishyenn@gmail.com>
2024-04-03 20:39:56 +0900

    [level 0] Title: 더 크게 합치기, Time: 9.49 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
-        int ab = Integer.parseInt(Integer.toString(a) + Integer.toString(b));
-        int ba = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
+        int ab = Integer.parseInt(""+a+b);
+        int ba = Integer.parseInt(""+b+a);
         return(Math.max(ab, ba));
     }
 }
