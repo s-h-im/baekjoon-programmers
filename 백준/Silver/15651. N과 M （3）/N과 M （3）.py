4e8f2e7c752eca5293b9a611b0ab53423c8fe243
Suhyen Im <ishyenn@gmail.com>
2024-06-25 03:34:22 +0900

    Update N과 M （3）.py

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,17 +1,13 @@
 import sys
 n, m = map(int, sys.stdin.readline().split(' '))
 
-selected = [0 for _ in range(m)]
-used = [0 for _ in range(n + 1)]
-def rec_func(k):
-    if k == m:
-        for x in selected:
-            sys.stdout.write(str(x) + ' ')
-        sys.stdout.write('\n')
-    else:
-        for cand in range(1, n + 1):
-            selected[k] = cand
-            rec_func(k + 1)
-            selected[k] = 0
+arr = [0 for _ in range(m)]
+def dfs(cnt):
+    if cnt == m:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(1, n + 1):
+        arr[cnt] = i
+        dfs(cnt + 1)
 
-rec_func(0)
+dfs(0)
