be3f0713915e1e0044e6e00c6d8a6720d4bbeea0
Suhyen Im <ishyenn@gmail.com>
2024-07-02 01:56:46 +0900

    [level 2] Title: 올바른 괄호, Time: 8.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,13 @@
 def solution(s):
-    answer = True
-    s = list(s)
-    stack = []
-    stack.append(s[0])
-    for i in range(1, len(s)):
-        if len(stack) > 0 and (stack[-1] == '(' and s[i] == ')'): 
-            stack.pop()
-        else:
-            stack.append(s[i])
-    if len(stack) > 0:
-        answer = False
-    return answer
+    st = list()
+    for c in s:
+        if c == '(':
+            st.append(c)
+
+        if c == ')':
+            try:
+                st.pop()
+            except IndexError:
+                return False
+
+    return len(st) == 0
