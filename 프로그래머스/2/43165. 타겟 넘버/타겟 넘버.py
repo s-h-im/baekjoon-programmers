bc93935eb889412fc58b0ef450eb49ebf606771b
Suhyen Im <ishyenn@gmail.com>
2024-07-09 15:41:15 +0900

    [level 2] Title: 타겟 넘버, Time: 1.55 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,10 @@
 from collections import deque
 
 def solution(numbers, target):
-    result = 0
-    dq = deque()  
-    dq.append((numbers[0], 1)) 
-    dq.append((-numbers[0], 1))
-    while dq:  
-        x = dq.popleft()  
-        if x[1] == len(numbers):
-            if x[0] == target:    
-                result += 1
-        else:
-            dq.append((x[0] + numbers[x[1]], x[1] + 1))  
-            dq.append((x[0] - numbers[x[1]], x[1] + 1))  
-    return result
+    q = deque([0])
+    for n in numbers:
+        for _ in range(len(q)):
+            x = q.popleft()
+            q.append(x + n)
+            q.append(x - n)
+    return q.count(target)
