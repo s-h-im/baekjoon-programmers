7636d32031c13c657c9b1c1556ab0df32e09fab4
Suhyen Im <ishyenn@gmail.com>
2024-07-09 16:11:11 +0900

    Update N과 M （1）.py

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,14 +1,16 @@
-import sys
-n, m = map(int, sys.stdin.readline().split(' '))
-
-arr = [0 for _ in range(m)]
-remain = [for _ in range(m)]
-def dfs(cnt):
-	if cnt = m:
-		print(' '.join(map(str, arr)))
-		return
-	for i in range(1, n + 1):
-		arr[cnt] = i
-		dfs(cnt + 1)
-
-dfs(0)
+n,m = list(map(int,input().split()))
+ 
+s = []
+ 
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(1, n + 1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+ 
+dfs()
