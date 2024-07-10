aa8d51c4b5c356f89f3490852c572ca5eb3308bc
Suhyen Im <ishyenn@gmail.com>
2024-07-10 16:21:29 +0900

    [level 2] Title: 더 맵게, Time: 1873.40 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(scoville, K):
+    cnt = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            cnt = -1
+            break
+        n1 = heapq.heappop(scoville)
+        n2 = heapq.heappop(scoville)
+        n3 = n1 + n2 * 2
+        heapq.heappush(scoville, n3)
+        cnt += 1
+    return cnt
